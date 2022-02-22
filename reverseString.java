e1fba738325d309d75128061a8b1bee556919d39
Jn <52610256+ododok@users.noreply.github.com>
2022-02-22 19:49:00 +0100

    Reverse a String
    We can reverse the order of a String with "StringBuffer"

---
+++ reverseString.java
@@ -0,0 +1,8 @@
+    public static String reverseString(String str1) {
+
+
+        StringBuffer sb = new StringBuffer(str1);
+        String str2 = sb.reverse().toString();
+
+        return str2;
+    }
